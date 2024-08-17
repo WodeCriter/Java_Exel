bfda24c577fcf85af8136eec68d09bda2ec18c60
Daniel <danielpbea@gmail.com>
2024-08-17 17:14:49 +0300

    added 2 implementations of expressions

---
+++ src/exel/engine/expressions/imp/PlusExpression.java
@@ -0,0 +1,28 @@
+package exel.engine.expressions.imp;
+
+import exel.engine.effectivevalue.api.EffectiveValue;
+import exel.engine.effectivevalue.imp.EffectiveValueImp;
+import exel.engine.expressions.api.Expression;
+import exel.engine.spreadsheet.cell.api.CellType;
+
+public class PlusExpression implements Expression {
+
+    private Expression left;
+    private Expression right;
+
+    public PlusExpression(Expression left, Expression right) {
+        this.left = left;
+        this.right = right;
+    }
+
+    @Override
+    public EffectiveValue eval() {
+        EffectiveValue leftValue = left.eval();
+        EffectiveValue rightValue = right.eval();
+        // do some checking... error handling...
+        //double result = (Double) leftValue.getValue() + (Double) rightValue.getValue();
+        double result = leftValue.extractValueWithExpectation(Double.class) + rightValue.extractValueWithExpectation(Double.class);
+
+        return new EffectiveValueImp(CellType.NUMERIC, result);
+    }
+}
---
+++ src/exel/engine/expressions/imp/UpperCaseExpression.java
@@ -0,0 +1,20 @@
+package exel.engine.expressions.imp;
+
+import exel.engine.effectivevalue.api.EffectiveValue;
+import exel.engine.effectivevalue.imp.EffectiveValueImp;
+import exel.engine.expressions.api.Expression;
+import exel.engine.spreadsheet.cell.api.CellType;
+
+public class UpperCaseExpression implements Expression {
+
+    private final String value;
+
+    public UpperCaseExpression(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public EffectiveValue eval() {
+        return new EffectiveValueImp(CellType.STRING, value.toUpperCase());
+    }
+}
