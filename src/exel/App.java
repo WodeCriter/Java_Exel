533dc820d467e77c9b4c31ef784c9b989785e147
Daniel <danielpbea@gmail.com>
2024-09-04 20:23:59 +0300

    added a new main for exel

---
+++ src/exel/App.java
@@ -0,0 +1,36 @@
+package exel;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            // Assuming the FXML file is named "MainScreen.fxml" and is located in the "app" directory under resources
+            Parent root = FXMLLoader.load(getClass().getResource("resources/app/index.fxml"));
+
+            // Setting the title of the stage (optional)
+            primaryStage.setTitle("Exel");
+
+            // Creating a scene object with the loaded layout
+            Scene scene = new Scene(root);
+
+            // Adding the scene to the stage
+            primaryStage.setScene(scene);
+
+            // Displaying the contents of the stage
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);  // Launch the JavaFX application
+    }
+}
