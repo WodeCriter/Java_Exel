920e2cbc3761f75aba5ddb345d613385cfa3aa5f
ItamarAlon <72469824+ItamarAlon@users.noreply.github.com>
2024-09-25 12:58:38 +0300

    working on sort popup

--- src/exel/userinterface/resources/app/popups/sort/SetSortScreenController.java
+++ src/exel/userinterface/resources/app/popups/sort/SetSortScreenController.java
@@ -2,22 +2,16 @@ package exel.userinterface.resources.app.popups.sort;
 
 import exel.engine.imp.EngineImp;
 import exel.eventsys.EventBus;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.geometry.HPos;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
+import javafx.scene.input.MouseEvent;
 
-import java.lang.invoke.SwitchPoint;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class SetSortScreenController
 {
@@ -28,19 +22,23 @@ public class SetSortScreenController
     @FXML
     private TextField cell1TextField, cell2TextField;
     @FXML
-    private ChoiceBox<String> columnsToChoose;
+    private ComboBox<String> mainColumnComboBox;
     @FXML
     private Label sortByLabel;
 
-    private List<String> colChoices = null;
-    private List<String> colsPicked;
+    private List<String> possibleColumnChoices = null;
+    //private List<String> pickedColumns;
+    private List<ComboBox<String>> allComboBoxes;
+    private Map<ComboBox<String>, String> boxToChosenColumn;
     private int rowIndex = 4;  // Track the current row index for new rows
 
     @FXML
     private void initialize()
     {
-        colsPicked = new LinkedList<>();
-        columnsToChoose.setOnAction(event-> whenChoiceBoxClicked());
+        //pickedColumns = new ArrayList<>(5);
+        allComboBoxes = new ArrayList<>(5);
+        allComboBoxes.add(mainColumnComboBox);
+        boxToChosenColumn = new HashMap<>();
     }
 
     public void setEventBus(EventBus eventBus) {
@@ -53,22 +51,24 @@ public class SetSortScreenController
         shiftRowsDown(rowIndex);
         // Create new content for the new row
         Label guideLabel = new Label("then by");
-        ChoiceBox<String> newChoiceBox = new ChoiceBox<>();
-        Label choiceBoxTitle = new Label("Column");
-        gridPane.addRow(rowIndex, guideLabel, newChoiceBox);
+        ComboBox<String> newComboBox = new ComboBox<>();
+        //Label choiceBoxTitle = new Label("Column");
+        gridPane.addRow(rowIndex, guideLabel, newComboBox);
 
-        newChoiceBox.setPrefWidth(columnsToChoose.getPrefWidth());
-        newChoiceBox.setPrefHeight(columnsToChoose.getPrefHeight());
+        newComboBox.setPrefWidth(mainColumnComboBox.getPrefWidth());
+        newComboBox.setPrefHeight(mainColumnComboBox.getPrefHeight());
         guideLabel.setPrefWidth(sortByLabel.getPrefWidth());
         guideLabel.setPrefHeight(sortByLabel.getPrefHeight());
         guideLabel.setAlignment(Pos.CENTER);
 
-        newChoiceBox.getItems().addAll(colChoices);
-        newChoiceBox.setOnAction(event->whenChoiceBoxClicked());
+//        if (possibleColumnChoices != null)
+//            newComboBox.getItems().addAll(possibleColumnChoices);
+        newComboBox.setOnAction(this::whenPickingAColumn);
+        allComboBoxes.add(newComboBox);
 
-        // Get the current stage and resize the window to fit new content
-        Stage stage = (Stage) gridPane.getScene().getWindow();
-        stage.sizeToScene();  // Resize window to fit the new content
+//        // Get the current stage and resize the window to fit new content
+//        Stage stage = (Stage) gridPane.getScene().getWindow();
+//        stage.sizeToScene();  // Resize window to fit the new content
 
         // Increment the rowIndex for the next row
         rowIndex++;
@@ -92,20 +92,67 @@ public class SetSortScreenController
     }
 
     @FXML
-    void addChoices(ActionEvent event)
+    public void whenClickingOnComboBox(MouseEvent event)
     {
-        if (!columnsToChoose.getItems().isEmpty())
-            return;
+        if (possibleColumnChoices == null)
+            possibleColumnChoices = EngineImp.getAllColumnsBetween2Cords(cell1TextField.getText(), cell2TextField.getText());
+        if (possibleColumnChoices != null)
+            mainColumnComboBox.getItems().addAll(possibleColumnChoices);
+    }
+    @FXML
+    private void whenPickingAColumn(ActionEvent event)
+    {
+        ComboBox<String> chosenComboBox = (ComboBox<String>) event.getSource();
+        String previousPickedColumn = boxToChosenColumn.get(chosenComboBox);
 
-        colChoices = EngineImp.getAllColumnsBetween2Cords(cell1TextField.getText(), cell2TextField.getText());
-        if (colChoices != null)
-            columnsToChoose.getItems().addAll(colChoices);
+        if (previousPickedColumn == null)
+        {
+            boxToChosenColumn.put(chosenComboBox, chosenComboBox.getValue());
+            ListIterator<ComboBox<String>> iterator = getIteratorPointingAtNextBoxInList(chosenComboBox);
+            if (iterator != null && iterator.hasNext())
+            {
+                List<String> choicesWithoutPickedChoice = chosenComboBox.getItems().stream().
+                        filter(column->!column.equals(chosenComboBox.getValue())).toList();
+                iterator.next().getItems().addAll(choicesWithoutPickedChoice);
+                while (iterator.hasNext())
+                {
+                    ComboBox<String> nextBox = iterator.next();
+                    nextBox.setValue(null);
+                    nextBox.getItems().clear();
+                }
+            }
+        }
+        else
+        {
+            boxToChosenColumn.put(chosenComboBox, chosenComboBox.getValue());
+            ListIterator<ComboBox<String>> iterator = getIteratorPointingAtNextBoxInList(chosenComboBox);
+            if (iterator != null && iterator.hasNext())
+            {
+                List<String> choicesWithoutPickedChoice = chosenComboBox.getItems().stream().
+                        filter(column->!column.equals(chosenComboBox.getValue())).toList();
+                choicesWithoutPickedChoice.add(previousPickedColumn);
+                iterator.next().getItems().addAll(choicesWithoutPickedChoice);
+                while (iterator.hasNext())
+                {
+                    ComboBox<String> nextBox = iterator.next();
+                    nextBox.setValue(null);
+                    nextBox.getItems().clear();
+                }
+            }
+            //Need to go to the next box, put back the previousPickedColumn in it, and remove chosenComboBox.getValue()
+        }
     }
 
-    private void whenChoiceBoxClicked()
+    private ListIterator<ComboBox<String>> getIteratorPointingAtNextBoxInList(ComboBox<String> box)
     {
-        colsPicked.add(columnsToChoose.getValue());
-        if (colChoices != null && !colChoices.isEmpty())
-            colChoices.remove(columnsToChoose.getValue());
+        ListIterator<ComboBox<String>> iterator = allComboBoxes.listIterator();
+
+        while (iterator.hasNext())
+        {
+            if (iterator.next() == box)
+                return iterator;
+        }
+
+        return null;
     }
 }
